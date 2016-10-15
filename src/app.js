0a011cbe15e0c1894f26a40397686251d740f2dc
dph <jankoller888@gmail.com>
2016-10-14 19:09:31 -0500

    auto pull request every 10s

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
  testdata2=null;
 (function () {
 'use strict'
-angular.module('RoutingApp',['ui.router']);
+
 
 angular.module('RoutingApp')
 .config(RoutesConfig)
@@ -15,14 +15,14 @@ angular.module('RoutingApp')
 
 
 
-EventDetailController.$inject=['EventListService'];
-function EventDetailController(EventListService){
+EventDetailController.$inject=['EventListService', 'UtilitiesService'];
+function EventDetailController(EventListService, UtilitiesService){
   var ctrl=this;
   ctrl.activeEvent = EventListService.getActiveEvent(); //$state.params.activeEvent;
-  ctrl.questions =[{tick:'8:40',tag:'how are you?'}]
+  ctrl.questions =[{tick:'8:40',tag:'how are you?'}];
   console.log(ctrl);
-  var t1='2016-10-12T17:40:51.000Z';
-  var t2 ='2016-10-12T17:45:51.000Z';
+  var t1=  new Date('Fri Oct 14 2016 12:00:05 GMT-0500').toISOString();  //new Date(ctrl.activeEvent.startTime).toISOString();
+  var t2 =  new Date('Fri Oct 14 2016 13:00:05 GMT-0500').toISOString();  //new Date(ctrl.activeEvent.endTime).toISOString();
 
   var promise = EventListService.getQAEvents(t1,t2);
 
@@ -38,23 +38,44 @@ function EventDetailController(EventListService){
 
 
 //creat an controller  for Schedule page
-ScheduleController.$inject = ['EventListService','$state'];
-function ScheduleController(EventListService,$state){
+ScheduleController.$inject = ['EventListService','$state','UtilitiesService'];
+function ScheduleController(EventListService,$state,UtilitiesService){
   var ctrl = this;
   ctrl.events= EventListService.getEventList();
   console.log('cotroller is running');
   console.log(ctrl);
+  var schedule = UtilitiesService.getScheduleformat(ctrl.events);
+  
+  var t1=   new Date().toISOString();//"2016-10-13T20:04:06.000Z";
+  var t2 =  new Date().toISOString();   //"2016-10-13T21:06:06.595Z";
 
-  var promise = EventListService.getStartStopEvents();
+  var promise=[];
+  setInterval(function(){
+
+  promise= EventListService.getStartStopEvents(t1,new Date().toISOString());
 
   promise.then(function (response) {
     console.log('promise done');
     testdata= response.data; // for test only
+    
+    testdata= UtilitiesService.convertToTimeTuple(testdata);
+    var matchedInd= null;
+    //update the timestamp
+    if (testdata.length){
+      t1 = new Date(testdata[testdata.length-1].Tend+100).toISOString(); //+100 to exculde the last stop event
+    }
+    
+    for (var i = 0; i < testdata.length; i++) {
+      matchedInd =UtilitiesService.findMatchEvent(testdata[i],schedule)
+      UtilitiesService.updateStartStopTime(ctrl.events[matchedInd],testdata[i]);
+      EventListService.setEventisDone(matchedInd);
+    };
    console.log (response);
   })
   .catch(function (error) {
     console.log("Something went terribly wrong.");
-  });
+  });},10*1000);
+  
   //for click event
   ctrl.gotoDetail= function (e){
     EventListService.setActiveEvent(e);
@@ -70,33 +91,26 @@ function EventListService($http,ApiBasePath,servAddr){
   var events =[]; // this will be share between controller
   var activeEvent = null;
   //put some example event in
-  var q={};
-  q.t1="2016-10-13T20:04:06.000Z";    //'2016-10-12T17:40:51.000Z';
-  q.t2 ="2016-10-13T21:06:06.595Z";//'2016-10-12T17:45:51.000Z';
-  q.isStart = '{"$exists":true}';
-  q.mask ={'_id':false,'androidID':true, 'maxDur':true,'tag':true,'recordDate':true,'isStart':true};
-  var strArr = [];
-  strArr = strArr.concat('{"recordDate":{"$gte":{"$date":"'+ q.t1+'"}, "$lte":{"$date":"'+q.t2+'"}}}');
-  strArr = strArr.concat('{"isStart":'+q.isStart+'}');
-  var postDat = '[{"$and":['+strArr.join(',')+']},'+ JSON.stringify(q.mask) + ']';                     //'{"$and":['+strArr.join(',')+']}';
+                  //'{"$and":['+strArr.join(',')+']}';
     // Construct the query string
   //var qStr = JSON.stringify({'dbname':DB, 'colname': EVENT, 'user': USER, 'passwd': PWD, 'classname': q.cname});
   var qStr='dbname=publicDb&colname=event&user=&passwd=publicPwd&classname=none';
 
   // add log start log stop for an event when capture form database
-  var baseTime =1477409400000; //8:30 on Oct 25th 2016
+  var baseTime = new Date().getTime();    //1477409400000; //8:30 on Oct 25th 2016
+  //add duration interval
   events=[{
     name:'Welcome',
     speaker:'Edward Lee, Director',
     startTime:baseTime,
-    endTime:baseTime+5*60*1000,
+    endTime:baseTime+ 2*60*1000,//5*60*1000,
     status:"not started",
     isdone: false
   },{
     name:'MARCO Perspective Stakeholder for TerraSwarm Annual Review',
     speaker:'Gil Vandentop (SRC)',
-    startTime:baseTime+5*60*1000,
-    endTime:baseTime+10*60*1000,
+    startTime:baseTime+2.5*60*1000,//5*60*1000,
+    endTime:baseTime+4*60*1000,//10*60*1000,
     status:"not started",
     isdone:false
   },{
@@ -130,7 +144,16 @@ function EventListService($http,ApiBasePath,servAddr){
   service.getActiveEvent = function(){
     return activeEvent;
   };
-  service.getStartStopEvents= function(){
+  service.getStartStopEvents= function(t1,t2){
+     var q={};
+      q.t1=t1// "2016-10-13T20:04:06.000Z";    //'2016-10-12T17:40:51.000Z';
+      q.t2 =t2 //"2016-10-13T21:06:06.595Z";//'2016-10-12T17:45:51.000Z';
+      q.isStart = '{"$exists":true}';
+      q.mask ={'_id':false,'androidID':true, 'maxDur':true,'tag':true,'recordDate':true,'isStart':true};
+      var strArr = [];
+      strArr = strArr.concat('{"recordDate":{"$gte":{"$date":"'+ q.t1+'"}, "$lte":{"$date":"'+q.t2+'"}}}');
+      strArr = strArr.concat('{"isStart":'+q.isStart+'}');
+      var postDat = '[{"$and":['+strArr.join(',')+']},'+ JSON.stringify(q.mask) + ']';    
     var response = $http({
       method: "POST",
       url: (servAddr + "/query?"+ qStr),
@@ -144,10 +167,11 @@ function EventListService($http,ApiBasePath,servAddr){
   q.t1=t1;
   q.t2=t2;
   q.androidID="b8a9953125a933af";
+  q.isStart = '{"$exists":false}';
   q.mask ={'_id':false,'androidID':true,'tag':true,'recordDate':true,'isStart':true};
   var strAtt2 =[];
   strAtt2 = strAtt2.concat('{"recordDate":{"$gte":{"$date":"'+ q.t1+'"}, "$lte":{"$date":"'+q.t2+'"}}}');
-  strAtt2 = strAtt2.concat('{"androidID":"'+q.androidID+'"}');
+  strAtt2 = strAtt2.concat('{"isStart":'+q.isStart+'}');
   var postDat1 = '[{"$and":['+strAtt2.join(',')+']},'+ JSON.stringify(q.mask) + ']';  
 
   var response = $http({
